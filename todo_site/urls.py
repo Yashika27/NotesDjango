e66fbb103bb96ce592afcd31ac336779d9326eeb
Yashika <yashika.lamba.phe19@itbhu.ac.in>
2020-09-08 14:01:46 +0530

    First Commit

---
+++ todo_site/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin 
+from django.urls import path
+from todo import views
+  
+urlpatterns = [ 
+    #####################home_page########################################### 
+    path('', views.index, name="todo"), 
+    ####################give id no. item_id name or item_id=i.id ############ 
+    path('del/', views.remove, name="del"),
+    path('add/', views.add, name="add"),
+    ######################################################################## 
+    path('admin/', admin.site.urls), 
+] 
